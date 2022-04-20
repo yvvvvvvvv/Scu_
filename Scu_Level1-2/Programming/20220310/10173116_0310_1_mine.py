986b23ced5c4534cba30f380869c572be54ab9fd
MURAFER <95864395+MURAFER@users.noreply.github.com>
2022-04-20 15:54:41 +0800

    Update 10173116_0310_1_mine.py

--- Scu_Level1-2/Programming/20220310/10173116_0310_1_mine.py
+++ Scu_Level1-2/Programming/20220310/10173116_0310_1_mine.py
@@ -5,20 +5,20 @@ def vip2(name,tel='沒有號碼'):
     vip_dict2={'NAME':name,'TEL':tel}
     return vip_dict2
 
+v={}
 while True:
-    v={}
     print('建立VIP資訊系統')
     id=input('請輸入ID:')
     name=input('請輸入姓名:')
     tel=input('請輸入電話號碼:')
     member=vip(id,name,tel)
     if v=={}:
-        print(member,'\n')
+        v.update(member)
+        print(v)
     else:
         if id in v:
             a=vip2(name,tel)
-            b=v[id]
-            dict(b).update(a)
+            v[id]=dict(v[id]).update(a) #還在失敗狀態
             print(v)
         else:
             v.update(member)
